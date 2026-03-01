31ce4227e14b5b2fa37d6e7f67c998ce80e5fc83
juanesklk <juan.viera@uao.edu.co>
2026-03-01 11:41:53 -0500

    nueva estructura de carpetas

--- scripts/Caso_1/analisis_exploratorio.R
+++ scripts/Caso_1/analisis_exploratorio.R
@@ -17,16 +17,6 @@ tail(df_rendimiento)
 
 
 # -------------------------------------------------------------------------
-# Limpieza de datos por columna
-# -------------------------------------------------------------------------
-
-# Se verificara la unicidad de cada registro para variables cualitativas, los
-# rangos logicos para las cuantitativas, posteriormente, normalizaremos valores
-# dejando strings en minusculas, cambio de idioma y de haber categorias mal 
-# escritas (ejemplo: en lugar de Data, Datta o por el estilo) cambiarlas a los
-# valores correspondientes
-
-# -------------------------------------------------------------------------
 # >>>> Columna anio: <<<<
 # -------------------------------------------------------------------------
 
@@ -36,10 +26,9 @@ unique(df_rendimiento$anio) # Sin cambios
 # >>>> Columna semestre <<<<
 # -------------------------------------------------------------------------
 
-unique(df_rendimiento$semestre)
+# Semestre
+unique(df_rendimiento$semestre) # Solo cambio a lowercase
 
-# cambio a lowecase
-df_rendimiento$semestre <- tolower(df_rendimiento$semestre)
 
 # -------------------------------------------------------------------------
 # >>>> Columna puntaje_final <<<<
@@ -48,6 +37,8 @@ df_rendimiento$semestre <- tolower(df_rendimiento$semestre)
 # Los puntajes, aunque tengan outliers, teniendo en cuenta que se tratan de puntajes
 # como una prueba icfes, estan dentro de lo que se considera fisicamente
 # posible (suponiendo que la escala va de 0 a 250)
+
+df_rendimiento %>% summarise(min = min(puntaje_final, na.rm = TRUE), max = max(puntaje_final, na.rm = TRUE))
 
 ggplot(df_rendimiento, aes(y = puntaje_final)) + geom_boxplot(fill = "pink", color = "red", width = 0.05) + 
   theme(axis.title.y = element_text(size = 15), axis.text.y = element_text(size = 15), axis.text.x = element_blank(), axis.ticks.x = element_blank()) + 
@@ -60,10 +51,6 @@ ggplot(df_rendimiento, aes(y = puntaje_final)) + geom_boxplot(fill = "pink", col
 # Revisamos los maximos y minimos de la columna
 df_rendimiento %>% summarise(min = min(horas_estudio, na.rm = TRUE), max = max(horas_estudio, na.rm = TRUE))
 
-# Como son horas de estudio semanales el rango fisicamente posible es [0, 168]
-# Marcamos como NAN los valores fuera del rango
-
-df_rendimiento$horas_estudio[df_rendimiento$horas_estudio < 0 | df_rendimiento$horas_estudio > 128] <- NA
 # -------------------------------------------------------------------------
 # >>>> Columna asistencia<<<<<
 # -------------------------------------------------------------------------
@@ -72,20 +59,11 @@ df_rendimiento$horas_estudio[df_rendimiento$horas_estudio < 0 | df_rendimiento$h
 
 df_rendimiento %>% summarise(min = min(asistencia, na.rm = TRUE), max = max(asistencia, na.rm = TRUE))
 
-# Como es un porcentaje, el rango estricto es [0, 100]
-# Marcamos como NAN los valores fuera de rango
-
-df_rendimiento$asistencia[df_rendimiento$asistencia < 0 | df_rendimiento$asistencia > 100] <- NA
-
 # -------------------------------------------------------------------------
 # >>>> Columna promedio_previo<<<<<
 # -------------------------------------------------------------------------
 
 df_rendimiento %>% summarise(min = min(promedio_previo, na.rm = TRUE), max = max(promedio_previo, na.rm = TRUE))
-
-# Rango segun diccionario de variables [0, 10]
-
-df_rendimiento$promedio_previo[df_rendimiento$promedio_previo < 0 | df_rendimiento$promedio_previo > 10] <- NA
 
 # -------------------------------------------------------------------------
 # >>>> Columna horas_sueno<<<<<
@@ -93,17 +71,12 @@ df_rendimiento$promedio_previo[df_rendimiento$promedio_previo < 0 | df_rendimien
 
 df_rendimiento %>% summarise(min = min(horas_sueno, na.rm = TRUE), max = max(horas_sueno, na.rm = TRUE))
 
-# aunque es seria tremendo outlier, debemos considerar un rango de [0, 24]
-
-df_rendimiento$horas_sueno[df_rendimiento$horas_sueno < 0 | df_rendimiento$horas_sueno > 24] <- NA
-
 # -------------------------------------------------------------------------
 # >>>> Columna edad<<<<<
 # -------------------------------------------------------------------------
 
 # Aunque sea raro, es un rango teoricamente posible 
 df_rendimiento %>% summarise(min = min(edad, na.rm = TRUE), max = max(edad, na.rm = TRUE))
-
 
 # -------------------------------------------------------------------------
 # >>>> Columna estres<<<<<
@@ -116,19 +89,11 @@ df_rendimiento %>% summarise(min = min(estres, na.rm = TRUE), max = max(estres, 
 ggplot(df_rendimiento, aes(x = estres)) + geom_histogram(bins = 60, fill = "coral", color = "black") + 
   theme_minimal() + labs(x = "Nivel de Estrés", y = "Frecuencia")
 
-# El rango puede plantearse en una escala de [0, 10]
-
-df_rendimiento$estres[df_rendimiento$estres < 0 | df_rendimiento$estres > 10] <- NA
-
 # -------------------------------------------------------------------------
 # >>>> Columna uso_redes<<<<<
 # -------------------------------------------------------------------------
 
 df_rendimiento %>% summarise(min = min(uso_redes, na.rm = TRUE), max = max(uso_redes, na.rm = TRUE))
-
-# Rango fisicamente posible [0, 24] - horas por dia
-
-df_rendimiento$uso_redes[df_rendimiento$uso_redes < 0 | df_rendimiento$uso_redes > 24] <- NA
 
 # -------------------------------------------------------------------------
 # >>>> Columna ingresos_familiares<<<<<
@@ -136,23 +101,11 @@ df_rendimiento$uso_redes[df_rendimiento$uso_redes < 0 | df_rendimiento$uso_redes
 
 df_rendimiento %>% summarise(min = min(ingresos_familiares, na.rm = TRUE), max = max(ingresos_familiares, na.rm = TRUE))
 
-# No es posible tener ingresos negativos por tanto [>0] USD/mes
-
-df_rendimiento$ingresos_familiares[df_rendimiento$ingresos_familiares < 0] <- NA
-
 # -------------------------------------------------------------------------
 # >>>> Columna genero<<<<<
 # -------------------------------------------------------------------------
 
 unique(df_rendimiento$genero) # Teniamos Male, Female, FEMALE y male
-
-# normalizamos
-df_rendimiento$genero <- tolower(df_rendimiento$genero)
-
-# cambiamos el idioma
-
-df_rendimiento$genero[df_rendimiento$genero == "female"] <- "mujer"
-df_rendimiento$genero[df_rendimiento$genero == "male"] <- "hombre"
 
 # -------------------------------------------------------------------------
 # >>>> Columna carrera<<<<<
@@ -160,51 +113,22 @@ df_rendimiento$genero[df_rendimiento$genero == "male"] <- "hombre"
 
 unique(df_rendimiento$carrera) # Teniamos Business y Busines
 
-# normalizamos
-df_rendimiento$carrera <- tolower(df_rendimiento$carrera)
-
-# Corregimos el error de tipado
-df_rendimiento$carrera[df_rendimiento$carrera == "busines"] <- "business"
-
-# Cambiamos el idioma
-df_rendimiento$carrera[df_rendimiento$carrera == "business"] <- "negocios" 
-df_rendimiento$carrera[df_rendimiento$carrera == "cs"] <- "ciencias de la computacion"
-df_rendimiento$carrera[df_rendimiento$carrera == "data"] <- "datos"
-
 # -------------------------------------------------------------------------
 # >>>> Columna acceso_internet<<<<<
 # -------------------------------------------------------------------------
 unique(df_rendimiento$acceso_internet) # tenemos NO, Yes, No, yes
 
-#normalizamos
-df_rendimiento$acceso_internet <- tolower(df_rendimiento$acceso_internet)
-
-# Dicotomizamos para futuro analisis [0 = no, 1 = si]
-df_rendimiento$acceso_internet[df_rendimiento$acceso_internet == "yes"] <- 1
-df_rendimiento$acceso_internet[df_rendimiento$acceso_internet == "no"] <- 0
 # -------------------------------------------------------------------------
 # >>>> Columna trabaja<<<<<
 # -------------------------------------------------------------------------
 
 unique(df_rendimiento$trabaja) # Teniamos No, Si, sí, NO
 
-# normalizamos
-df_rendimiento$trabaja <- tolower(df_rendimiento$trabaja)
-df_rendimiento$trabaja[df_rendimiento$trabaja == "sí"] <- "si"
-
-# Dicotomizamos para posterior analisis
-df_rendimiento$trabaja[df_rendimiento$trabaja == "si"] <- 1
-df_rendimiento$trabaja[df_rendimiento$trabaja == "no"] <- 0
-
-
 # -------------------------------------------------------------------------
 # >>>> Columna modalidad<<<<<
 # -------------------------------------------------------------------------
 
 unique(df_rendimiento$modalidad)
-
-# Normalizamos
-df_rendimiento$modalidad <- tolower(df_rendimiento$modalidad)
 
 # -------------------------------------------------------------------------
 # Revision de datos faltantes
---
+++ scripts/Caso_1/limpieza.R
@@ -0,0 +1,100 @@
+
+df_rendimiento <- read.csv(here("data", "processed", "rendimiento_2021-2025_sem1&sem2.csv"))
+
+# -------------------------------------------------------------------------
+# Limpieza de datos por columna
+# -------------------------------------------------------------------------
+
+# Se verificara la unicidad de cada registro para variables cualitativas, los
+# rangos logicos para las cuantitativas, posteriormente, normalizaremos valores
+# dejando strings en minusculas, cambio de idioma y de haber categorias mal 
+# escritas (ejemplo: en lugar de Data, Datta o por el estilo) cambiarlas a los
+# valores correspondientes
+
+# Semestre
+df_rendimiento$semestre <- tolower(df_rendimiento$semestre)
+
+# Como son horas de estudio semanales el rango fisicamente posible es [0, 168]
+# Marcamos como NAN los valores fuera del rango
+
+# Horas de estudio
+df_rendimiento$horas_estudio[df_rendimiento$horas_estudio < 0 | df_rendimiento$horas_estudio > 128] <- NA
+
+# asistencia
+
+# Como es un porcentaje, el rango estricto es [0, 100]
+# Marcamos como NAN los valores fuera de rango
+
+df_rendimiento$asistencia[df_rendimiento$asistencia < 0 | df_rendimiento$asistencia > 100] <- NA
+
+
+# Promedio previo
+# Rango segun diccionario de variables [0, 10]
+
+df_rendimiento$promedio_previo[df_rendimiento$promedio_previo < 0 | df_rendimiento$promedio_previo > 10] <- NA
+
+# Horas de sueño
+# aunque es seria tremendo outlier, debemos considerar un rango de [0, 24]
+
+df_rendimiento$horas_sueno[df_rendimiento$horas_sueno < 0 | df_rendimiento$horas_sueno > 24] <- NA
+
+# Estres
+# El rango puede plantearse en una escala de [0, 10]
+
+df_rendimiento$estres[df_rendimiento$estres < 0 | df_rendimiento$estres > 10] <- NA
+
+# Uso de redes
+# Rango fisicamente posible [0, 24] - horas por dia
+
+df_rendimiento$uso_redes[df_rendimiento$uso_redes < 0 | df_rendimiento$uso_redes > 24] <- NA
+
+# Ingresos familiares
+# No es posible tener ingresos negativos por tanto [>0] USD/mes
+
+df_rendimiento$ingresos_familiares[df_rendimiento$ingresos_familiares < 0] <- NA
+
+# GENERO
+# normalizamos
+df_rendimiento$genero <- tolower(df_rendimiento$genero)
+
+# Dicotomizamos para posterior analisis
+
+df_rendimiento$genero[df_rendimiento$genero == "female"] <- 1
+df_rendimiento$genero[df_rendimiento$genero == "male"] <- 0
+
+
+# Carrera
+# normalizamos
+df_rendimiento$carrera <- tolower(df_rendimiento$carrera)
+
+# Corregimos el error de tipado
+df_rendimiento$carrera[df_rendimiento$carrera == "busines"] <- "business"
+
+# Cambiamos el idioma
+df_rendimiento$carrera[df_rendimiento$carrera == "business"] <- "negocios" 
+df_rendimiento$carrera[df_rendimiento$carrera == "cs"] <- "ciencias de la computacion"
+df_rendimiento$carrera[df_rendimiento$carrera == "data"] <- "datos"
+
+# ACCESO A INTERNET
+#normalizamos
+df_rendimiento$acceso_internet <- tolower(df_rendimiento$acceso_internet)
+
+# Dicotomizamos para futuro analisis [0 = no, 1 = si]
+df_rendimiento$acceso_internet[df_rendimiento$acceso_internet == "yes"] <- 1
+df_rendimiento$acceso_internet[df_rendimiento$acceso_internet == "no"] <- 0
+
+# Trabaja
+# normalizamos
+df_rendimiento$trabaja <- tolower(df_rendimiento$trabaja)
+df_rendimiento$trabaja[df_rendimiento$trabaja == "sí"] <- "si"
+
+# Dicotomizamos para posterior analisis
+df_rendimiento$trabaja[df_rendimiento$trabaja == "si"] <- 1
+df_rendimiento$trabaja[df_rendimiento$trabaja == "no"] <- 0
+
+# Modalidad
+# Normalizamos
+df_rendimiento$modalidad <- tolower(df_rendimiento$modalidad)
+
+# 
+
