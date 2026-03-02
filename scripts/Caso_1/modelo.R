17881a4ba10782de57b75ab02587515a35f937d2
juanesklk <juan.viera@uao.edu.co>
2026-03-01 20:39:13 -0500

    prueba con variables numericas

--- scripts/Caso_1/modelo.R
+++ scripts/Caso_1/modelo.R
@@ -1,10 +1,29 @@
 library(corrplot)
-
+library(gtsummary)
 # -------------------------------------------------------------------------
 # ANOVA
 # -------------------------------------------------------------------------
 
+# Cargamos los datos imputados
 df_anova_rendimiento <- read.csv(here("data", "processed", "rendimiento_imputado.csv"))
 
+# matriz de correlacion 
 
+# Seleccionamos las variables cuantitativas
+num_df_AR <- df_anova_rendimiento %>% select(puntaje_final, horas_estudio, 
+                                             asistencia, horas_sueno, 
+                                             promedio_previo, edad, estres, 
+                                             uso_redes, ingresos_familiares)
 
+cor_df <- cor(num_df_AR)
+corrplot(cor_df, method = "number", type = "lower")
+
+# Interaccion entre variables categoricas
+
+# Modelo de prueba solo con variables numericas
+modelo_1 <- lm(puntaje_final ~ horas_estudio + asistencia +promedio_previo + 
+               horas_sueno + edad + estres + uso_redes + ingresos_familiares,
+               data = num_df_AR)
+
+options(scipen = 999)
+summary(modelo_1)
